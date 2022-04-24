ff6810f1036a82f9f997c905d072b8e16882228d
Sebastian Otalora <sebastian.otarola@almundo.com>
2022-04-24 12:34:13 -0500

    03 - Ext Question

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -37,6 +37,26 @@
         return true;
 
     }
+
+    // Ejercicio Extra
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result;
+        if (isDead) {
+            result = 1500;
+        } else {
+            if (isSeparated) {
+                result = 2500;
+            } else {
+                if (isRetired) {
+                    result = 3000;
+                } else {
+                    result = 4000;
+                }
+            }
+        }
+
+        return result;
+    }
 })();
 
 
