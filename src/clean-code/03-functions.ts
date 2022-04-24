a7476ac0a480ea7728dea0a3a405304702f9e44d
Sebastian Otalora <sebastian.otarola@almundo.com>
2022-04-24 12:27:33 -0500

    03 Solved

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,41 +1,42 @@
 (() => {
 
     // función para obtener información de una película por Id
-    function getAllMovies(movieId: string) {
-        console.log({ movieId });
-    }
-
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors(id: string) {
+    function getMovieById(id: string) {
         console.log({ id });
     }
 
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieCastById(movieId: string) {
+        console.log({ movieId });
+    }
+
     // funcion para obtener el bio del actor por el id
-    function getUsuario(ActorId: string) {
+    function getActorBiographyById(ActorId: string) {
         console.log({ ActorId });
     }
 
     // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[]) {
+    interface createMovieOptions { title: string, description: string, rating: number, cast: string[] }
+    function createMovie({ title, description, rating, cast }: createMovieOptions) {
         console.log({ title, description, rating, cast });
     }
 
     // Crea un nuevo actor
-    function createActorIfActorNotExists(fullName: string, birthdate: Date): boolean {
+    function alreadyExistActor(actorFullName: String) {
+        if (actorFullName === 'fernando') return false;
+        return true;
+    }
+    function createActor(fullName: string): boolean {
 
         // tarea asincrona para verificar nombre
         // ..
         // ..
-        if (fullName === 'fernando') return false;
+        alreadyExistActor(fullName);
 
         console.log('Crear actor');
         return true;
 
     }
-
-
-
-
 })();
 
 
